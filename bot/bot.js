1f56c8e58468ec978629547a0f9b06e081aac8fb
Ian Moon <imm3350@g.rit.edu>
2019-11-05 22:59:14 -0500

    Discord Bot
    Adding the code to Github

--- bot/bot.js
+++ bot/bot.js
@@ -8,109 +8,187 @@ const debug = false;
 const token = process.env.TOKEN;
 
 const diceCommands = {
-  '!highest': command.RollHighest,
-  '!roll': command.RollDice,
+    '!highest': command.RollHighest,
+    '!roll': command.RollDice,
 };
 const otherCommands = {
-  '!help': command.help,
-  '!ban': command.ban,
-  '!unban': command.unban,
-  '!banlist': command.banList,
-  '!admin': command.admin,
-  '!revoke': command.unadmin,
-  '!adminlist': command.adminList,
+    '!help': command.help,
+    '!ban': command.ban,
+    '!unban': command.unban,
+    '!banlist': command.banList,
+    '!admin': command.admin,
+    '!revoke': command.unadmin,
+    '!adminlist': command.adminList,
+};
+
+const customCommands = {
+
 };
 
 const greetings = {
-  PoloSpankin: "Oh... he's here. Don't let him roll me.",
-  Vandush: 'The mighty king has returned to his throne to bless the land with his presence! Hoozah! Three cheers for Van! Hip-Hip-Hooray! Hip-Hip-Hooray! Hip-Hip-Hooray! Roll me dear master!',
-  Ian: 'Creator! owo',
-  THELAGPYRO: "Act natural act natural... we've done nothing wrong.",
+    //Jonny
+    222039245685129217: "Oh... he's here. Don't let him roll me.",
+    //Van
+    225430614260711424: 'The mighty king has returned to his throne to bless the land with his presence! Hoozah! Three cheers for Van! Hip-Hip-Hooray! Hip-Hip-Hooray! Hip-Hip-Hooray! Roll me dear master!',
+    //Ian
+    251911037178085376: 'Creator! owo',
+    //Nick J
+    308410594002731008: "Act natural act natural... we've done nothing wrong.",
+    //Merrick
+    300095616116195330: "I've never been so happy! ヽ(*￣o￣*)ノ♩♫♪"
 };
 
 const leaving = {
-  PoloSpankin: 'Phew. That was a close one.',
-  Vandush: 'I miss him already..',
-  Ian: 'What am I without my creator? omo',
-  THELAGPYRO: "Okay, don't panic, alright? Stop panicking!",
+    //Jonny
+    222039245685129217: 'Phew. That was a close one.',
+    //Van
+    225430614260711424: 'I miss him already..',
+    //Ian
+    251911037178085376: 'What am I without my creator? omo',
+    //Nick J
+    308410594002731008: "Okay, don't panic, alright? Stop panicking!",
+    //Merrick
+    300095616116195330: "I've never experienced such crushing sadness.. (⌣̩̩́_⌣̩̩̀)"
 };
 
 client.on('ready', () => {
-  if (debug) {
-    command.bannedUsers.Ian = "I'm sorry Master.. omo";
-  }
+    if (debug) {
+        command.bannedUsers.Ian = "I'm sorry Master.. omo";
+    }
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-  const newUserChannel = newMember.voiceChannel;
-  const oldUserChannel = oldMember.voiceChannel;
-  const channel = newMember.guild.channels.find(ch => ch.name === 'nsfw-globalmemedomination');
-  if (!channel) return;
+    const newUserChannel = newMember.voiceChannel;
+    const oldUserChannel = oldMember.voiceChannel;
+    const channel = newMember.guild.channels.find(ch => ch.name === 'nsfw-globalmemedomination');
+    if (!channel) return;
 
-  if (oldUserChannel === undefined && newUserChannel !== undefined) {
-    if (greetings[newMember.user.username]) {
-          // User Joins a voice channel
-      channel.send(greetings[newMember.user.username]);
-    }
-  } else if (newUserChannel === undefined && leaving[newMember.user.username]) {
+    if (oldUserChannel === undefined && newUserChannel !== undefined) {
+        if (greetings[newMember.user.username]) {
+            // User Joins a voice channel
+            channel.send(greetings[newMember.user.username]);
+        }
+    } else if (newUserChannel === undefined && leaving[newMember.user.username]) {
         // User leaves a voice channel
-    channel.send(leaving[newMember.user.username]);
-  }
+        channel.send(leaving[newMember.user.username]);
+    }
 });
 
 client.on('message', msg => {
-  const content = msg.content;
+    const content = msg.content;
 
-  if (content[0] === '!') {
-    let firstSpace = content.indexOf(' ');
-    if (firstSpace === -1) {
-      firstSpace = content.length;
-    }
-    const firstWord = content.substr(0, firstSpace);
-    let results = '';
-    let response = '\n';
-    let user = '';
-    const author = msg.author.username;
-
-    if (diceCommands[firstWord]) {
-      const dPos = content.toLowerCase().indexOf('d');
-      const num = content.slice(firstSpace + 1, dPos);
-      const size = parseInt(content.slice(dPos + 1, content.length), 10);
-      results = diceCommands[firstWord](author, num, size);
-      const length = Object.keys(results).length;
-
-      for (let i = 0; i < length - 1; i++) {
-        response += `Die ${i + 1}: ${results[i]}\n`;
-      }
-
-      response += `Result: ${results.result}`;
-
-      return msg.reply(response);
-    } else if (otherCommands[firstWord]) {
-      user = content.slice(firstSpace + 1, content.length);
-      response = otherCommands[firstWord](user, author, client);
-
-      if (response.embed) {
-        const length = Object.keys(response).length;
-        const embed = new Discord.RichEmbed();
-        embed.setTitle('Help');
-        embed.setColor(3447003);
-        embed.setDescription('This is a list of commands as well as examples of how to use them.');
-        embed.setTimestamp();
-
-        for (let i = 0; i < length - 1; i++) {
-          embed.addField(response[i].name, `${response[i].desc}\nExample: ${response[i].usage}`);
+    if (content[0] === '!') {
+        let firstSpace = content.indexOf(' ');
+        if (firstSpace === -1) {
+            firstSpace = content.length;
         }
-        return msg.channel.send({
-          embed,
-        });
-      }
+        const firstWord = content.substr(0, firstSpace);
+        let results = '';
+        let response = '\n';
+        let user = '';
+        const author = msg.author.username;
+        const authorID = msg.author.id;
 
-      return msg.channel.send(response);
+        if (diceCommands[firstWord]) {
+            const dPos = content.toLowerCase().indexOf('d');
+            const num = parseInt(content.slice(firstSpace + 1, dPos), 10);
+            let mod = 0;
+            let modPos = content.length;
+            if (content.includes("+")) {
+                modPos = content.toLowerCase().indexOf('+');
+            } else if (content.includes("-")) {
+                modPos = content.toLowerCase().indexOf('-');
+            }
+            const size = parseInt(content.slice(dPos + 1, modPos), 10);
+            if (modPos != content.length) {
+                mod = parseInt(content.slice(modPos + 1, modPos), 10);
+            }
+            results = diceCommands[firstWord](author, num, size, mod);
+            const length = Object.keys(results).length;
+
+            for (let i = 0; i < length - 1; i++) {
+                response += `Die ${i + 1}: ${results[i]}\n`;
+            }
+
+            response += `Result: ${results.result}`;
+
+            return msg.reply(response);
+        } else if (otherCommands[firstWord]) {
+            user = content.slice(firstSpace + 1, content.length);
+            response = otherCommands[firstWord](user, author, client);
+
+            if (response.embed) {
+                const length = Object.keys(response).length;
+                const embed = new Discord.RichEmbed();
+                embed.setTitle('Help');
+                embed.setColor(3447003);
+                embed.setDescription('This is a list of commands as well as examples of how to use them.');
+                embed.setTimestamp();
+
+                for (let i = 0; i < length - 1; i++) {
+                    embed.addField(response[i].name, `${response[i].desc}\nExample: ${response[i].usage}`);
+                }
+                return msg.channel.send({
+                    embed,
+                });
+            }
+
+            return msg.channel.send(response);
+        } else if (firstword == "!create") {
+            const authorID = msg.author.id;
+            const secondspace = content.indexOf(' ', str.indexOf(' ') + 1);
+            const name = content.substr(firstSpace + 1, secondspace);
+            const purpose = content.substr(secondspace + 1, content.length);
+
+            if (!customCommands.hasOwnProperty(authorID)) {
+                customCommands[authorID] = {};
+            }
+
+            customCommands[authorID][firstWord] = purpose;
+
+        } else if (customCommands[msg.author.id][firstword]) {
+            contentMsg = customCommands[msg.author.id][firstword];
+            const dPos = contentMsg.toLowerCase().indexOf('d');
+            const num = parseInt(contentMsg.slice(firstSpace + 1, dPos), 10);
+            let mod = 0;
+            let modPos = contentMsg.length;
+            if (contentMsg.includes("+")) {
+                modPos = contentMsg.toLowerCase().indexOf('+');
+            } else if (contentMsg.includes("-")) {
+                modPos = contentMsg.toLowerCase().indexOf('-');
+            }
+            const size = parseInt(contentMsg.slice(dPos + 1, modPos), 10);
+            if (modPos != contentMsg.length) {
+                mod = parseInt(contentMsg.slice(modPos + 1, modPos), 10);
+            }
+            results = diceCommands[firstWord](author, num, size, mod);
+            const length = Object.keys(results).length;
+
+            for (let i = 0; i < length - 1; i++) {
+                response += `Die ${i + 1}: ${results[i]}\n`;
+            }
+
+            response += `Result: ${results.result}`;
+
+            return msg.reply(response);
+        } else if (firstword == "!commands") {
+            const authorID = msg.author.id;
+
+            if (!customCommands.hasOwnProperty(authorID)) {
+                response += `You don't have any custom commands, create one using !create.`
+            } else {
+                response += `Here are your custom commands: `
+                for (var keys in json) {
+                    response += keys + `: ` + customCommands[authorID][keys] + `\n`;
+                }
+
+                return msg.reply(response);
+            }
+        }
+
+        return msg.reply(`Invalid Command. (${firstWord}) >-<`);
     }
-    return msg.reply(`Invalid Command. (${firstWord}) >-<`);
-  }
-  return false;
+    return false;
 });
 
 client.login(token);
--- bot/commands.js
+++ bot/commands.js
@@ -5,239 +5,253 @@ const bannedUsers = {
 };
 
 const adminUsers = {
-  Ian: 'Praise Ian or perish... (/◕ヮ◕)/',
+    //Ian
+    251911037178085376: 'Praise Ian or perish... (/◕ヮ◕)/',
 };
 
-const RollDice = (user, num, size) => {
-  const result = {
-    result: 0,
-  };
+const RollDice = (user, num, size, mod) => {
+    const result = {
+        result: 0,
+    };
 
-  if (bannedUsers[user]) {
-    result.result = bannedUsers[user];
+    if (bannedUsers[user]) {
+        result.result = bannedUsers[user];
+
+        return result;
+    }
+
+    if (isNaN(num) || isNaN(size)) {
+        result.result = 'Passed in number and size values must be whole numbers. Example: 1d12+0';
+        return result;
+    } else if (num <= 1 || size < 1 || num > 20 || size > 100 || mod < -10 || mod > 10) {
+        result.result = 'Invalid size or number. Format Example: XdY+Z where X is 1-20, Y is 2-100, and Z is -10 - 10';
+        return result;
+    }
+
+    for (let i = 0; i < num; i++) {
+        const randomResult = random.int(1, size);
+        result[i] = randomResult;
+        result.result += randomResult;
+    }
+
+    result.result += mod;
 
     return result;
-  }
-
-  if (isNaN(num) || isNaN(size)) {
-    result.result = 'Passed in number and size values must be integers';
-    return result;
-  } else if (num < 1 || size < 1 || num > 20 || size > 100) {
-    result.result = 'Invalid size or number. Format Example: XdY where X is 1-20 and Y is 2-100';
-    return result;
-  }
-
-  for (let i = 0; i < num; i++) {
-    const randomResult = random.int(1, size);
-    result[i] = randomResult;
-    result.result += randomResult;
-  }
-
-  return result;
 };
 
 const rollDiceMeta = () => {
-  const meta = {
-    name: '!roll',
-    desc: 'Gets the total of rolling X die of Y size',
-    usage: '!roll 2d20',
-  };
+    const meta = {
+        name: '!roll',
+        desc: 'Gets the total of rolling X die of Y size with a modifier of Z',
+        usage: '!roll 2d20+0',
+    };
 
-  return meta;
+    return meta;
 };
 
-const RollHighest = (user, num, size) => {
-  const result = {
-    result: 0,
-  };
+const RollHighest = (user, num, size, mod) => {
+    const result = {
+        result: 0,
+    };
 
-  if (bannedUsers[user]) {
-    result.result = bannedUsers[user];
+    if (bannedUsers[user]) {
+        result.result = bannedUsers[user];
 
-    return result;
-  }
-
-  if (isNaN(num) || isNaN(size)) {
-    result.result = 'Passed in number and size values must be whole numbers. Example: 1d12';
-    return result;
-  } else if (num < 1 || size < 1 || num > 20 || size > 100) {
-    result.result = 'Invalid size or number. Format Example: XdY where X is 1-20 and Y is 2-100';
-    return result;
-  }
-
-  for (let i = 0; i < num; i++) {
-    const randomResult = random.int(1, size);
-    result[i] = randomResult;
-    if (randomResult > result.result) {
-      result.result = randomResult;
+        return result;
     }
-  }
 
-  return result;
+    if (isNaN(num) || isNaN(size)) {
+        result.result = 'Passed in number and size values must be whole numbers. Example: 1d12+0';
+        return result;
+    } else if (num <= 1 || size < 1 || num > 20 || size > 100 || mod < -10 || mod > 10) {
+        result.result = 'Invalid size or number. Format Example: XdY+Z where X is 1-20, Y is 2-100, and Z is -10 - 10';
+        return result;
+    }
+
+    for (let i = 0; i < num; i++) {
+        const randomResult = random.int(1, size) + mod;
+        result[i] = randomResult;
+        if (randomResult > result.result) {
+            result.result = randomResult;
+        }
+    }
+
+    return result;
 };
 
 const rollHighestMeta = () => {
-  const meta = {
-    name: '!highest',
-    desc: 'Gets the highest roll of X die of Y size',
-    usage: '!highest 2d20',
-  };
+    const meta = {
+        name: '!highest',
+        desc: 'Gets the highest roll of X die of Y size and a modifier of Z',
+        usage: '!highest 2d20+0',
+    };
 
-  return meta;
+    return meta;
+};
+
+const createCustomMeta = (user, purpose) => {
+    const meta = {
+        name: '!create',
+        desc: "Creates/Overwrites a custom function that you can reuse by invoking its' name",
+        usage: '!create fireball 1d10 (reused by typing !fireball)',
+    };
+
+    return meta;
 };
 
 const ban = (user, author, client) => {
-  let response = "They've already been taken care of. (✿◠‿◠)";
-  if (!adminUsers[author]) {
-    return 'You\'re not my master! <(｀^´)>';
-  } else if (!client.users.find('username', user)) {
-    response = 'Invalid username';
-  } else if (bannedUsers[user]) {
-    response = 'That person is already banned!';
-  } else if (user === 'Ian') {
-    response = 'I will not ban my creator!';
-  } else {
-      bannedUsers[user] = `No more bad touch, ${user}! (╬ ಠ益ಠ)`;
-      if(adminUsers[user]) {
-          delete adminUsers[user];
-      }
-      response = `No more bad touch, ${user}! (╬ ಠ益ಠ)`;
-  }
+    let response = "They've already been taken care of. (✿◠‿◠)";
+    if (!adminUsers[author]) {
+        return 'You\'re not my master! <(｀^´)>';
+    } else if (!client.users.find('username', user)) {
+        response = 'Invalid username';
+    } else if (bannedUsers[user]) {
+        response = 'That person is already banned!';
+    } else if (user === 'Ian') {
+        response = 'I will not ban my creator!';
+    } else {
+        bannedUsers[user] = `No more bad touch, ${user}! (╬ ಠ益ಠ)`;
+        if (adminUsers[user]) {
+            delete adminUsers[user];
+        }
+        response = `No more bad touch, ${user}! (╬ ಠ益ಠ)`;
+    }
 
-  return response;
+    return response;
 };
 
 const banMeta = () => {
-  const meta = {
-    name: '!ban',
-    desc: 'Stops a person from rolling.',
-    usage: '!ban Name',
-  };
+    const meta = {
+        name: '!ban',
+        desc: 'Stops a person from rolling',
+        usage: '!ban Name',
+    };
 
-  return meta;
+    return meta;
 };
 
 const unban = (user, author, client) => {
-  let response = 'You were already able to roll me! (╬ ಠ益ಠ)';
-  if (!adminUsers[author]) {
-    return 'You\'re not my master! <(｀^´)>';
-  } else if (!client.users.find('username', user)) {
-    response = 'Invalid username';
-  } else if (!bannedUsers[user]) {
-    response = 'That person is already unbanned!';
-  } else {
-    delete bannedUsers[user];
-    response = `You can roll me now, ${user}... (´･ω･\`)`;
-  }
+    let response = 'You were already able to roll me! (╬ ಠ益ಠ)';
+    if (!adminUsers[author]) {
+        return 'You\'re not my master! <(｀^´)>';
+    } else if (!client.users.find('username', user)) {
+        response = 'Invalid username';
+    } else if (!bannedUsers[user]) {
+        response = 'That person is already unbanned!';
+    } else {
+        delete bannedUsers[user];
+        response = `You can roll me now, ${user}... (´･ω･\`)`;
+    }
 
-  return response;
+    return response;
 };
 
 const unbanMeta = () => {
-  const meta = {
-    name: '!unban',
-    desc: 'Allows a person to person to rolling.',
-    usage: '!unban Name',
-  };
+    const meta = {
+        name: '!unban',
+        desc: 'Allows a person to person to rolling.',
+        usage: '!unban Name',
+    };
 
-  return meta;
+    return meta;
 };
 
 const banList = () => {
-  let response = 'No one has been purged yet! owo';
-  if (Object.keys(bannedUsers).length > 0) {
-    response = '\nThose currently purged:';
-    const keys = Object.keys(bannedUsers);
-    for (let i = 0; i < keys.length; i++) {
-      response += `\n${keys[i]}`;
+    let response = 'No one has been purged yet! owo';
+    if (Object.keys(bannedUsers).length > 0) {
+        response = '\nThose currently purged:';
+        const keys = Object.keys(bannedUsers);
+        for (let i = 0; i < keys.length; i++) {
+            response += `\n${keys[i]}`;
+        }
     }
-  }
 
-  return response;
+    return response;
 };
 
 const banListMeta = () => {
-  const meta = {
-    name: '!banlist',
-    desc: "View who's been purged... ;_;",
-    usage: '!banlist',
-  };
+    const meta = {
+        name: '!banlist',
+        desc: "View who's been purged... ;_;",
+        usage: '!banlist',
+    };
 
-  return meta;
+    return meta;
 };
 
 const admin = (user, author, client) => {
-  let response = "They've already been taken care of. (✿◠‿◠)";
-  if (!adminUsers[author]) {
-    return 'You\'re not my master! <(｀^´)>';
-  } else if (!client.users.find('username', user)) {
-    response = 'Invalid username';
-  } else if (adminUsers[user]) {
-    response = 'That person has already arisen!';
-  } else {
-      if(bannedUsers[user]) {
-          delete bannedUsers[user];
-      }
-      adminUsers[user] = `Praise ${user} or perish... (/◕ヮ◕)/`;
-      response = `Praise ${user} or perish... (/◕ヮ◕)/`;
-  }
+    let response = "They've already been taken care of. (✿◠‿◠)";
+    if (!adminUsers[author]) {
+        return 'You\'re not my master! <(｀^´)>';
+    } else if (!client.users.find('username', user)) {
+        response = 'Invalid username';
+    } else if (adminUsers[user]) {
+        response = 'That person has already arisen!';
+    } else {
+        if (bannedUsers[user]) {
+            delete bannedUsers[user];
+        }
+        adminUsers[user] = `Praise ${user} or perish... (/◕ヮ◕)/`;
+        response = `Praise ${user} or perish... (/◕ヮ◕)/`;
+    }
 
-  return response;
+    return response;
 };
 
 const unadmin = (user, author, client) => {
-  let response = 'You were already able to roll me! (╬ ಠ益ಠ)';
-  if (!adminUsers[author]) {
-    return 'You\'re not my master! <(｀^´)>';
-  } else if (!client.users.find('username', user)) {
-    response = 'Invalid username';
-  } else if (!adminUsers[user]) {
-    response = 'That person hasn\'t ascended!';
-  } else if (user === 'Ian') {
-    response = 'I will not harm my creator!';
-  } else {
-    delete adminUsers[user];
-    response = `You have no power here ${user}.. (╬ ಠ益ಠ)`;
-  }
+    let response = 'You were already able to roll me! (╬ ಠ益ಠ)';
+    if (!adminUsers[author]) {
+        return 'You\'re not my master! <(｀^´)>';
+    } else if (!client.users.find('username', user)) {
+        response = 'Invalid username';
+    } else if (!adminUsers[user]) {
+        response = 'That person hasn\'t ascended!';
+    } else if (user === 'Ian') {
+        response = 'I will not harm my creator!';
+    } else {
+        delete adminUsers[user];
+        response = `You have no power here ${user}.. (╬ ಠ益ಠ)`;
+    }
 
-  return response;
+    return response;
 };
 
 const adminList = () => {
-  let response = 'No one has ascended! (╬ ಠ益ಠ)';
-  if (Object.keys(adminUsers).length > 0) {
-    response = '\nThose currently ascended:';
-    const keys = Object.keys(adminUsers);
-    for (let i = 0; i < keys.length; i++) {
-      response += `\n${keys[i]}`;
+    let response = 'No one has ascended! (╬ ಠ益ಠ)';
+    if (Object.keys(adminUsers).length > 0) {
+        response = '\nThose currently ascended:';
+        const keys = Object.keys(adminUsers);
+        for (let i = 0; i < keys.length; i++) {
+            response += `\n${keys[i]}`;
+        }
     }
-  }
 
-  return response;
+    return response;
 };
 
 const adminListMeta = () => {
-  const meta = {
-    name: '!adminlist',
-    desc: "(/◕ヮ◕)/ View who's been ascended! (/◕ヮ◕)/",
-    usage: '!adminlist',
-  };
+    const meta = {
+        name: '!adminlist',
+        desc: "(/◕ヮ◕)/ View who's been ascended! (/◕ヮ◕)/",
+        usage: '!adminlist',
+    };
 
-  return meta;
+    return meta;
 };
 
 const help = () => {
-  const results = {
-    0: rollHighestMeta(),
-    1: rollDiceMeta(),
-    2: banMeta(),
-    3: unbanMeta(),
-    4: banListMeta(),
-    5: adminListMeta(),
-    embed: true,
-  };
+    const results = {
+        0: rollHighestMeta(),
+        1: rollDiceMeta(),
+        2: createCustomMeta(),
+        3: banMeta(),
+        4: unbanMeta(),
+        5: banListMeta(),
+        6: adminListMeta(),
+        embed: true,
+    };
 
-  return results;
+    return results;
 };
 
 module.exports.RollDice = RollDice;
