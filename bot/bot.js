eb6952aec76419d4569aeffd6a5a19b7e627494f
Ian Moon <imm3350@g.rit.edu>
2019-11-06 20:09:52 -0500

    Added catch case clause
    Added a clause for if universal commands somehow get deleted

--- bot/bot.js
+++ bot/bot.js
@@ -258,7 +258,7 @@ client.on('message', msg => {
         } else if (firstWord == "!commands") {
             //console.log("Listing commands...");
             //console.log("Have ID?: " + !customCommands.hasOwnProperty(authorID))
-            if (!customCommands.hasOwnProperty(authorID) || Object.keys(customCommands[authorID]).length == 0) {
+            if (!customCommands.hasOwnProperty(authorID) || Object.keys(customCommands[authorID]).length == 0 || Object.keys(customCommands["universal"]).length == 0) {
                 response += `You don't have any custom commands. See !help.`;
             } else {
                 response += `Here are your custom commands: \nUniversal: \n`;
